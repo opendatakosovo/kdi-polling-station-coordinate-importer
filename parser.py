91ec21d7c727506047de7afacaa5708e0664d8b0
endrit-b <endriti.92@gmail.com>
2014-07-22 15:13:12 +0200

    added coordinates to documents in mongo

--- parser.py
+++ parser.py
@@ -15,15 +15,25 @@ def result():
 	with open('qendrat_votimit.csv', 'rb') as csvfile:
 		reader = csv.reader(csvfile, delimiter=',')
 		for row in reader:
+			lat = row[4]
+			lon = row[5]
+			
 			stationNumber=row[6]
+			print(stationNumber + ': ' + lat + ' - ' + lon)
 			cursor = collection.find({'pollingStation.number':stationNumber})
 	
 			if cursor.count() == 0:
 				print("not matched " + stationNumber)
 			elif cursor.count() == 1:
-				print(cursor[0]['pollingStation'])
+				
+				doc_id=cursor[0]['_id']
+				collection.update({'_id':doc_id},{'$set':{'pollingStation.coordinates':{'lat':lat,'lon':lon}}})
+				print(cursor[0]['_id'])
 				
 			else:
 				print("Error: Multiple Match")
 
 result()
+
+
+
